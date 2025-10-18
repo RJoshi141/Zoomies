e1d41260d58c945559f9d9308449b3e31f6d9daa
Ritika Joshi <ritikajoshi141@gmail.com>
2025-10-18 14:22:28 -0700

    Initial Commit

---
+++ ZoomiesTests/ZoomiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomiesTests.swift
+//  ZoomiesTests
+//
+//  Created by Ritika Joshi on 10/18/25.
+//
+
+import Testing
+@testable import Zoomies
+
+struct ZoomiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
